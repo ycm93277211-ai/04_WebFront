cabfa66c340ccfea4515d24798f6f01c7be050b9
ycm93277211-ai <ycm93277211@gmail.com>
2025-11-16 19:39:43 +0900

    dd

--- 3_Javascript/js/크기,색상,내용 지정해서 등록하기.js
+++ 3_Javascript/js/크기,색상,내용 지정해서 등록하기.js
@@ -4,29 +4,58 @@ const inputResult =document.querySelector("#inputResult");
 const size =document.querySelector("#size");
 const color =document.querySelector("#color");
 
-let size1;
-let color1;
+let sum = "";
 
+document.querySelector("#btn1").addEventListener("click",()=>{
 
-document.querySelector("#btn1").addEventListener("click",function(){
-  
-  // 저장값
-  const size1 = document.querySelector("#size").value;
-  const color1 = document.querySelector("#color").value;
+  textarea.style.fontSize=size.value + "px";
+  textarea.style.color=color.value;
 
-  textarea.style.fontSize=size1 + "px";
-  textarea.style.color=color1;
-
-  
 });
-
 
 document.querySelector("#btn2").addEventListener("click",()=>{
-  const text = textarea.value;
-  inputResult.innerText = text;
+  
+  sum += textarea.value;
 
-  inputResult.style.fontSize=size1 + "px";
-  inputResult.style.color=color1;
+  inputResult.innerHTML = sum;
 
-
+  inputResult.style.fontSize = size.value + "px";
+  inputResult.style.color = color.value;
 });
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------
+
+// let size1;
+// let color1;
+
+
+// document.querySelector("#btn1").addEventListener("click",function(){
+  
+//   // 저장값
+//   const size1 = document.querySelector("#size").value;
+//   const color1 = document.querySelector("#color").value;
+
+//   textarea.style.fontSize=size1 + "px";
+//   textarea.style.color=color1;
+
+  
+// });
+
+
+// document.querySelector("#btn2").addEventListener("click",()=>{
+//   const text = textarea.value;
+//   inputResult.innerText = text;
+
+//   inputResult.style.fontSize=size1 + "px";
+//   inputResult.style.color=color1;
+
+
+// });
